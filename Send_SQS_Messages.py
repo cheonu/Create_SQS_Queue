8ae3a2218fe9aea3f1feec613b20b6d254d2e286
chedev <cheonu85@gmail.com>
2020-06-23 12:46:14 -0400

    send SQS messages

---
+++ Send_SQS_Messages.py
@@ -0,0 +1,11 @@
+import boto3
+sqs=boto3.resource("sqs")
+#sending messages on AWS SQS
+queue = sqs.get_queue_by_name(
+    QueueName='test')
+
+response = queue.send_message(
+    MessageBody='Sending a nice message')
+
+print(response.get("MessageId"))
+print(response.get("MD5OfMessageBody"))
